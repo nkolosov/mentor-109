e51b282ff5a340ba45cc4883f2e21a28b5e42c2c
ilamaksimov <Maksimov.I@citilink.ru>
2023-01-21 14:58:29 +0300

    добавлен http server

---
+++ category/internal/api/http/service/server.go
@@ -0,0 +1,74 @@
+package service
+
+import (
+	"context"
+	"github.com/buaazp/fasthttprouter"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/valyala/fasthttp"
+	"github.com/valyala/fasthttp/fasthttpadaptor"
+	"go.uber.org/zap"
+	"net/http/pprof"
+)
+
+const pprofUrlPrefix = "/debug/pprof"
+
+type Server struct {
+	logger *zap.Logger
+}
+
+func NewServer(logger *zap.Logger) *Server {
+	return &Server{
+		logger: logger,
+	}
+}
+
+func (s *Server) ListenAndServe(ctx context.Context, listen string, enablePprof bool) error {
+	router := fasthttprouter.New()
+
+	router.GET("/healthz", s.HandleLivenessProbe)
+	router.GET("/healthy", s.HandleReadinessProbe)
+	router.GET("/metrics", s.HandleMetrics)
+	// обязательный путь для проверки валидной работы самого HTTP-сервера
+	router.GET("/ping", s.HandlePing)
+
+	if enablePprof {
+		for _, path := range []string{"/", "/allocs", "/block", "/goroutine", "/heap", "/mutex", "/threadcreate"} {
+			router.GET(pprofUrlPrefix+path, fasthttpadaptor.NewFastHTTPHandlerFunc(pprof.Index))
+		}
+		router.GET(pprofUrlPrefix+"/cmdline", fasthttpadaptor.NewFastHTTPHandlerFunc(pprof.Cmdline))
+		router.GET(pprofUrlPrefix+"/profile", fasthttpadaptor.NewFastHTTPHandlerFunc(pprof.Profile))
+		router.GET(pprofUrlPrefix+"/symbol", fasthttpadaptor.NewFastHTTPHandlerFunc(pprof.Symbol))
+		router.GET(pprofUrlPrefix+"/trace", fasthttpadaptor.NewFastHTTPHandlerFunc(pprof.Trace))
+		s.logger.Warn("pprof routes registered", zap.String("name", pprofUrlPrefix))
+	}
+
+	server := &fasthttp.Server{
+		Handler: router.Handler,
+	}
+
+	go func() {
+		<-ctx.Done()
+		err := server.Shutdown()
+		if err != nil {
+			s.logger.Error("server shutdown with error", zap.Error(err))
+		}
+	}()
+	return server.ListenAndServe(listen)
+}
+
+func (s *Server) HandleLivenessProbe(ctx *fasthttp.RequestCtx) {
+	ctx.SuccessString("text/plain; charset=utf-8", "OK")
+}
+
+func (s *Server) HandleReadinessProbe(ctx *fasthttp.RequestCtx) {
+	ctx.SuccessString("text/plain; charset=utf-8", "OK")
+}
+
+func (s *Server) HandleMetrics(ctx *fasthttp.RequestCtx) {
+	prometheusHandler := fasthttpadaptor.NewFastHTTPHandler(promhttp.Handler())
+	prometheusHandler(ctx)
+}
+
+func (s *Server) HandlePing(ctx *fasthttp.RequestCtx) {
+	ctx.SuccessString("text/plain; charset=utf-8", "PONG")
+}
--- category/internal/config/config.go
+++ category/internal/config/config.go
@@ -2,10 +2,14 @@ package config
 
 // Config конфигурация приложения
 type Config struct {
-	LogLevel          string `long:"log-level" description:"Log level: panic, fatal, warn or warning, info, debug" env:"CL_LOG_LEVEL" required:"true"`
-	LogJSON           bool   `long:"log-json" description:"Enable force log format JSON" env:"CL_LOG_JSON"`
-	HttpPrivateListen string `long:"http-private-listen" description:"Listening host:port for private http-server" env:"CL_HTTP_PRIVATE_LISTEN" required:"true"`
+	LogLevel string `long:"log-level" description:"Log level: panic, fatal, warn or warning, info, debug" env:"CL_LOG_LEVEL" required:"true"`
+	LogJSON  bool   `long:"log-json" description:"Enable force log format JSON" env:"CL_LOG_JSON"`
+	// HttpServiceListen хост и порт, который будет слушать HTTP сервер (в формате host:port)
+	HttpServiceListen string `long:"http-service-listen" description:"Listening host:port for service http-server" env:"CL_HTTP_SERVICE_LISTEN" required:"true"`
 	GrpcListen        string `long:"grpc-listen" description:"Listening host:port for grpc-server" env:"CL_GRPC_LISTEN" required:"true"`
+
+	// EnablePprof включение отладки при помощи pprof
+	EnablePprof bool `long:"enable-pprof" description:"Enable pprof server" env:"CL_ENABLE_PPROF"`
 
 	DBHost     string `long:"db-host" description:"Database Server" env:"CL_DB_HOST" required:"true"`
 	DBPort     int    `long:"db-port" description:"Database Port" env:"CL_DB_PORT" required:"true"`
